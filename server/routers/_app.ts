98e0b10e2dabb2509b71c10cdc16b7c86e40788d
Kaartik Shukla <kaartikshukla7120@gmail.com>
2023-03-16 17:16:53 +0530

    Added setup files for trpc

--- server/routers/_app.ts
+++ server/routers/_app.ts
@@ -1,3 +1,4 @@
+import { generateRandomString } from "@/utils/util";
 import { z } from "zod";
 import { procedure, router } from "../trpc";
 
@@ -6,7 +7,76 @@ export const appRouter = router({
         input(z.string())
         .query(({ input }) => {
             return `hello ${input}`;
-        })
+        }),
+    login: procedure.input(z.object({
+        response_type: z.string().startsWith("code"),
+        client_id: z.string(),
+        scope: z.string(),
+        redirect_uri: z.string(),
+    })).query(({ input, ctx }) => {
+
+        const state = generateRandomString(16);
+        ctx.res.redirect("https://accounts.spotify.com/authorize?" + JSON.stringify({
+            ...input,
+            state,
+        }));
+    }),
+    callback: procedure.input(z.object({
+        code: z.string(),
+        state: z.string(),
+    })).query(async ({ input, ctx }) => {
+        const client_id = '4de79c018e8242fbbe08481a01821bda';
+        const client_secret = '4f0ce72cc24c4764a6cc4c5b8adb545d';
+
+        const authOptions = {
+            url: 'https://accounts.spotify.com/api/token',
+            headers: {
+            },
+            form: {
+                grant_type: 'client_credentials'
+            },
+            json: true
+        };
+
+        try {
+            const query = await fetch('https://accounts.spotify.com/api/token', {
+                headers: {
+                    'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64')),
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                },
+                body: 'grant_type=client_credentials',
+                method: 'POST',
+            });
+
+            const result = await query.json();
+            return result;
+
+        } catch (error) {
+            throw error;
+        }
+    }),
+    getMusic: procedure.input(z.object({
+        id: z.string(),
+        token: z.string(),
+    })).query(async ({ input }) => {
+        try {
+            const query = await fetch(`https://api.spotify.com/v1/tracks/${input.id}`, {
+                headers: {
+                    'Authorization': `Bearer ${input.token}`,
+                    'Content-Type': 'application/json',
+                }
+            })
+
+            const result = await query.json();
+            if (result.href) {
+                return result.href;
+            }
+            
+            return null;
+        } catch (error) {
+            throw error;
+        }
+    }),
 })
 
 export type AppRouter = typeof appRouter;
--- server/trpc.ts
+++ server/trpc.ts
@@ -1,6 +1,15 @@
 import { initTRPC } from '@trpc/server';
+import { CreateNextContextOptions } from '@trpc/server/adapters/next';
 
-const t = initTRPC.create();
+export const createContext = (opts: CreateNextContextOptions) => {
+    return {
+        headers: opts.req.headers,
+        req: opts.req,
+        res: opts.res,
+    };
+}
+
+const t = initTRPC.context<typeof createContext>().create();
 
 export const router = t.router;
 export const procedure = t.procedure;
