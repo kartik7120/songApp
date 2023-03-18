85057f7d16c8c436797100f5c225577ee6813435
Kaartik Shukla <kaartikshukla7120@gmail.com>
2023-03-18 11:04:28 +0530

    Added a new page for creating new posts

---
+++ pages/new.tsx
@@ -0,0 +1,14 @@
+import CreatePost from "@/components/CreatePost";
+
+export default function NewPost() {
+    return (
+        <>
+            <div>
+                <CreatePost />
+            </div>
+            <div>
+                Second Section
+            </div>
+        </>
+    )
+}
