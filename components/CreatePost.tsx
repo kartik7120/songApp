b779ffd0366789fde97f381d9e030d8de2fa2cd7
Kaartik Shukla <kaartikshukla7120@gmail.com>
2023-03-20 16:42:53 +0530

    Added react hook form for form feilds

--- components/CreatePost.tsx
+++ components/CreatePost.tsx
@@ -7,6 +7,8 @@ import rehypeSanitize from "rehype-sanitize";
 import { useMantineColorScheme } from "@mantine/core";
 import { useState } from "react";
 import styles from "../styles/post.module.scss";
+import { Controller } from "react-hook-form";
+import { useForm, SubmitHandler, SubmitErrorHandler } from "react-hook-form";
 
 const MDEditor = dynamic(
     () => import("@uiw/react-md-editor"),
@@ -20,51 +22,85 @@ interface Props {
     setEditorFocused: React.Dispatch<React.SetStateAction<boolean>>
 }
 
+interface FormValues {
+    image_file: File | null;
+    title: string;
+    tags: string[];
+    body: string;
+}
+
 export default function CreatePost(props: Props) {
+
+    const { control, formState: { errors }, watch, handleSubmit, reset } = useForm<FormValues>({
+        defaultValues: {
+            body: "",
+            image_file: null,
+            tags: [],
+            title: "",
+        }
+    });
+
     const tags = Array.from({ length: 10 }, (_, i) => ({
         value: `tag-${i}`,
         label: `Tag ${i}`,
     }));
 
-    const [value, setValue] = useState<string | undefined>(undefined);
-
     const theme = useMantineColorScheme();
 
     function handleReset() {
-        setValue(undefined);
+        reset();
+    }
+
+    const handleFormSubmit: SubmitHandler<FormValues> = (data) => {
+        console.log(`data = ${JSON.stringify(data)}`);
+        console.log(`image_file = ${JSON.stringify(data.image_file)}`);
+    }
+
+    const onError: SubmitErrorHandler<FormValues> = (errors, e) => {
+        console.log(`errors = ${JSON.stringify(errors)}`);
+        console.log(`e = ${JSON.stringify(e)}`);
     }
 
     return (
         <div className={styles.container}>
-            <Paper shadow="md" p="md" withBorder>
-                <FileInput pb="lg" placeholder="Upload a cover image" style={{ width: "12em" }} accept="image/jpeg image/png image/svg+xml image/webp" size="md" />
-                <TextInput onFocus={() => {
-                    props.setEditorFocused(false);
-                    props.setTagsFocused(false);
-                    props.setInputFocused(true);
-                }} className={styles.inputClass} mb="lg" placeholder="New post title here..." variant="filled" withAsterisk size="xl" />
-                <MultiSelect onFocus={() => {
-                    props.setEditorFocused(false);
-                    props.setInputFocused(false);
-                    props.setTagsFocused(true);
-                }} mb="lg" icon={<HiOutlineHashtag />} data={tags} placeholder="Add up to 4 tags" size="md"
-                    clearable maxDropdownHeight={300} maxSelectedValues={4} transitionProps={{
-                        duration: 150, transition: "pop-top-left"
-                    }} />
-                <div data-color-mode={theme.colorScheme}>
-                    <div className="wmde-markdown-var"> </div>
-                    <MDEditor value={value} onChange={setValue} previewOptions={{ rehypePlugins: [[rehypeSanitize]] }} onFocus={() => {
-                        props.setTagsFocused(false);
-                        props.setInputFocused(false);
-                        props.setEditorFocused(true);
-                    }} />
-                </div>
-                <Group spacing="md" align="center" mt="lg">
-                    <Button variant="filled" radius="md" color="violet" size="md">Publish</Button>
-                    <Button variant="subtle" radius="md" size="md">Save Draft</Button>
-                    <Button variant="subtle" onClick={handleReset} radius="md" color="indigo" size="md">Revert New Changes</Button>
-                </Group>
-            </Paper>
+            <form onSubmit={handleSubmit(handleFormSubmit, onError)}>
+                <Paper shadow="md" p="md" withBorder>
+                    <Controller name="image_file" control={control} render={({ field }) => (
+                        <FileInput {...field} pb="lg" placeholder="Upload a cover image" style={{ width: "12em" }} accept="image/jpeg,image/png,image/svg+xml,image/webp" size="md" />
+                    )} />
+                    <Controller name="title" control={control} render={({ field }) => (
+                        <TextInput {...field} onFocus={() => {
+                            props.setEditorFocused(false);
+                            props.setTagsFocused(false);
+                            props.setInputFocused(true);
+                        }} className={styles.inputClass} mb="lg"
+                            placeholder="New post title here..."
+                            variant="filled" withAsterisk size="xl" />
+                    )} />
+                    <Controller name="tags" control={control} render={({ field }) => (
+                        <MultiSelect {...field} onFocus={() => {
+                            props.setEditorFocused(false);
+                            props.setInputFocused(false);
+                            props.setTagsFocused(true);
+                        }} mb="lg" icon={<HiOutlineHashtag />} data={tags} placeholder="Add up to 4 tags" size="md"
+                            clearable maxDropdownHeight={300} maxSelectedValues={4} transitionProps={{
+                                duration: 150, transition: "pop-top-left"
+                            }} />
+                    )} />
+                    <Controller name="body" control={control} render={({ field }) => (
+                        <MDEditor {...field} previewOptions={{ rehypePlugins: [[rehypeSanitize]] }} onFocus={() => {
+                            props.setTagsFocused(false);
+                            props.setInputFocused(false);
+                            props.setEditorFocused(true);
+                        }} />
+                    )} />
+                    <Group spacing="md" align="center" mt="lg">
+                        <Button variant="filled" type="submit" radius="md" color="violet" size="md">Publish</Button>
+                        <Button variant="subtle" radius="md" size="md">Save Draft</Button>
+                        <Button variant="subtle" onClick={handleReset} radius="md" color="indigo" size="md">Revert New Changes</Button>
+                    </Group>
+                </Paper>
+            </form>
         </div>
     );
 }
