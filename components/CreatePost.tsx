e94de590fe9703031c3185a5081c570087a3bbeb
Kaartik Shukla <kaartikshukla7120@gmail.com>
2023-03-20 17:25:48 +0530

    added a modal for reseting the state to nearest save state

--- components/CreatePost.tsx
+++ components/CreatePost.tsx
@@ -1,4 +1,4 @@
-import { Button, FileInput, Group, MultiSelect, Paper, TextInput } from "@mantine/core";
+import { Button, FileInput, Group, Modal, MultiSelect, Paper, TextInput } from "@mantine/core";
 import { HiOutlineHashtag } from "react-icons/hi";
 import "@uiw/react-md-editor/markdown-editor.css";
 import "@uiw/react-markdown-preview/markdown.css";
@@ -9,6 +9,8 @@ import { useState } from "react";
 import styles from "../styles/post.module.scss";
 import { Controller } from "react-hook-form";
 import { useForm, SubmitHandler, SubmitErrorHandler } from "react-hook-form";
+import { BsUpload } from "react-icons/bs";
+import { useDisclosure } from "@mantine/hooks";
 
 const MDEditor = dynamic(
     () => import("@uiw/react-md-editor"),
@@ -45,6 +47,8 @@ export default function CreatePost(props: Props) {
         label: `Tag ${i}`,
     }));
 
+    const [opened, { open, close }] = useDisclosure(false);
+
     const theme = useMantineColorScheme();
 
     function handleReset() {
@@ -62,45 +66,59 @@ export default function CreatePost(props: Props) {
     }
 
     return (
-        <div className={styles.container}>
-            <form onSubmit={handleSubmit(handleFormSubmit, onError)}>
-                <Paper shadow="md" p="md" withBorder>
-                    <Controller name="image_file" control={control} render={({ field }) => (
-                        <FileInput {...field} pb="lg" placeholder="Upload a cover image" style={{ width: "12em" }} accept="image/jpeg,image/png,image/svg+xml,image/webp" size="md" />
-                    )} />
-                    <Controller name="title" control={control} render={({ field }) => (
-                        <TextInput {...field} onFocus={() => {
-                            props.setEditorFocused(false);
-                            props.setTagsFocused(false);
-                            props.setInputFocused(true);
-                        }} className={styles.inputClass} mb="lg"
-                            placeholder="New post title here..."
-                            variant="filled" withAsterisk size="xl" />
-                    )} />
-                    <Controller name="tags" control={control} render={({ field }) => (
-                        <MultiSelect {...field} onFocus={() => {
-                            props.setEditorFocused(false);
-                            props.setInputFocused(false);
-                            props.setTagsFocused(true);
-                        }} mb="lg" icon={<HiOutlineHashtag />} data={tags} placeholder="Add up to 4 tags" size="md"
-                            clearable maxDropdownHeight={300} maxSelectedValues={4} transitionProps={{
-                                duration: 150, transition: "pop-top-left"
+        <>
+            <Modal opened={opened} centered onClose={close} title="Are you sure you want to revert to the previous save ?" withCloseButton>
+                <Group spacing="md" align="center" mt="lg">
+                    <Button variant="filled" color="red" onClick={() => {
+                        handleReset();
+                        close();
+                    }}>
+                        Yes
+                    </Button>
+                    <Button variant="filled" color="blue" onClick={close}>No</Button>
+                </Group>
+            </Modal>
+            <div className={styles.container}>
+                <form onSubmit={handleSubmit(handleFormSubmit, onError)}>
+                    <Paper shadow="md" p="md" withBorder>
+                        <Controller name="image_file" control={control} render={({ field }) => (
+                            <FileInput icon={<BsUpload />} {...field} pb="lg" placeholder="Upload a cover image"
+                                style={{ width: "15em" }} accept="image/jpeg,image/png,image/svg+xml,image/webp" size="md" />
+                        )} />
+                        <Controller name="title" control={control} render={({ field }) => (
+                            <TextInput {...field} onFocus={() => {
+                                props.setEditorFocused(false);
+                                props.setTagsFocused(false);
+                                props.setInputFocused(true);
+                            }} className={styles.inputClass} mb="lg"
+                                placeholder="New post title here..."
+                                variant="filled" withAsterisk size="xl" />
+                        )} />
+                        <Controller name="tags" control={control} render={({ field }) => (
+                            <MultiSelect {...field} onFocus={() => {
+                                props.setEditorFocused(false);
+                                props.setInputFocused(false);
+                                props.setTagsFocused(true);
+                            }} mb="lg" icon={<HiOutlineHashtag />} data={tags} placeholder="Add up to 4 tags" size="md"
+                                clearable maxDropdownHeight={300} maxSelectedValues={4} transitionProps={{
+                                    duration: 150, transition: "pop-top-left"
+                                }} />
+                        )} />
+                        <Controller name="body" control={control} render={({ field }) => (
+                            <MDEditor {...field} previewOptions={{ rehypePlugins: [[rehypeSanitize]] }} onFocus={() => {
+                                props.setTagsFocused(false);
+                                props.setInputFocused(false);
+                                props.setEditorFocused(true);
                             }} />
-                    )} />
-                    <Controller name="body" control={control} render={({ field }) => (
-                        <MDEditor {...field} previewOptions={{ rehypePlugins: [[rehypeSanitize]] }} onFocus={() => {
-                            props.setTagsFocused(false);
-                            props.setInputFocused(false);
-                            props.setEditorFocused(true);
-                        }} />
-                    )} />
-                    <Group spacing="md" align="center" mt="lg">
-                        <Button variant="filled" type="submit" radius="md" color="violet" size="md">Publish</Button>
-                        <Button variant="subtle" radius="md" size="md">Save Draft</Button>
-                        <Button variant="subtle" onClick={handleReset} radius="md" color="indigo" size="md">Revert New Changes</Button>
-                    </Group>
-                </Paper>
-            </form>
-        </div>
+                        )} />
+                        <Group spacing="md" align="center" mt="lg">
+                            <Button variant="filled" type="submit" radius="md" color="violet" size="md">Publish</Button>
+                            <Button variant="subtle" radius="md" size="md">Save Draft</Button>
+                            <Button variant="subtle" onClick={open} radius="md" color="indigo" size="md">Revert New Changes</Button>
+                        </Group>
+                    </Paper>
+                </form>
+            </div>
+        </>
     );
 }
