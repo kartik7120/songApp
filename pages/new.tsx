6f3c23c42179bfc4678642efcee0868883a5cac0
Kaartik Shukla <kaartikshukla7120@gmail.com>
2023-03-20 21:41:00 +0530

    Added a table component

--- pages/new.tsx
+++ pages/new.tsx
@@ -4,6 +4,7 @@ import styles from "../styles/new.module.scss";
 import { useRef, useEffect, useState, useReducer } from "react";
 import { useDisclosure } from "@mantine/hooks";
 import { IoMdArrowDropdown, IoMdArrowDropright } from "react-icons/io";
+import CreatePostMarkDownTable from "@/components/CreatePostMarkDownTable";
 
 export default function NewPost() {
     const firstRef = useRef<{ isFocused: boolean }>({ isFocused: false });
@@ -17,7 +18,7 @@ export default function NewPost() {
             <div className={styles.container2}>
                 <CreatePost setEditorFocused={setEditorFocused} setTagsFocused={setTagsFocused} setInputFocused={setFocused} firstRef={firstRef} />
             </div>
-            <div>
+            <div className={styles.container3}>
                 <Transition keepMounted={true} mounted={focused} transition="fade" duration={300} timingFunction="ease">
                     {(styles) => (
                         <div style={styles} className="first_section">
@@ -64,140 +65,7 @@ export default function NewPost() {
                                 </Group>
                             </Text>
                             <Collapse in={opened}>
-                                <Table>
-                                    <tr>
-                                        <th>
-                                            Markdown
-                                        </th>
-                                        <th>
-                                            Formatted Text
-                                        </th>
-                                    </tr>
-                                    <tr>
-                                        <td>
-                                            <pre>
-                                                # Header
-                                                <br />
-                                                ...
-                                                <br />
-                                                ###### Header
-                                            </pre>
-                                        </td>
-                                        <td>
-                                            <pre>
-                                                H1 Header
-                                                <br />
-                                                ...
-                                                <br />
-                                                H6 Header
-                                            </pre>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td>
-                                            <pre>
-                                                *italics*
-                                                <br />
-                                                or
-                                                <br />
-                                                _italics_
-                                            </pre>
-                                        </td>
-                                        <td>
-                                            <i>italics</i>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td>
-                                            <pre>
-                                                **bold**
-                                            </pre>
-                                        </td>
-                                        <td>
-                                            <strong>
-                                                bold
-                                            </strong>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td>
-                                            <pre>
-                                                [Link](https://...)
-                                            </pre>
-                                        </td>
-                                        <td>
-                                            <a href="www.google.com">link</a>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td>
-                                            <pre>
-                                                * item 1
-                                                <br />
-                                                * item 2
-                                            </pre>
-                                        </td>
-                                        <td>
-                                            item 1
-                                            <br />
-                                            item 2
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td>
-                                            <pre>
-                                                1. item 1
-                                                <br />
-                                                2. item 2
-                                            </pre>
-                                        </td>
-                                        <td>
-                                            item 1
-                                            <br />
-                                            item 2
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td>
-                                            <pre>
-                                                &gt; quoted text
-                                            </pre>
-                                        </td>
-                                        <td>
-                                            |  quoted text
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td>
-                                            <pre>
-                                                `inline code`
-                                            </pre>
-                                        </td>
-                                        <td>
-                                            <Badge color="gray">Inline Code</Badge>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td>
-                                            <pre>
-                                                ```
-                                                <br />
-                                                code block
-                                                <br />
-                                                ```
-                                            </pre>
-                                        </td>
-                                        <td>
-                                            <div style={{
-                                                background: "black", width: 150, height: 80,
-                                                display: "flex", justifyContent: "center",
-                                                alignItems: "center", borderRadius: "10px"
-                                            }}>
-                                                <code>code block</code>
-                                            </div>
-                                        </td>
-                                    </tr>
-                                </Table>
+                                <CreatePostMarkDownTable />
                             </Collapse>
                         </div>
                     )}
