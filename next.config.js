9204bac836d3eee5fe88d32d8c2a4f9e9771ce8c
Kaartik Shukla <kaartikshukla7120@gmail.com>
2023-03-26 15:26:09 +0530

    Removed some config for mdx files

--- next.config.js
+++ next.config.js
@@ -1,16 +1,7 @@
-const withMDX = require('@next/mdx')({
-  extension: /\.mdx?$/,
-  options: {
-    remarkPlugins: [],
-    rehypePlugins: [],
-    providerImportSource: '@mdx-js/react',
-  },
-})
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
   reactStrictMode: true,
 }
 
-module.exports = withMDX(nextConfig);
+module.exports = nextConfig;
