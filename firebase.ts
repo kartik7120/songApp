b3e30938ddf94f1daad2969ad3790d7c0fc60693
Kaartik Shukla <kaartikshukla7120@gmail.com>
2023-03-26 15:25:50 +0530

    Added firebase cloud storage

--- firebase.ts
+++ firebase.ts
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { getStorage } from "firebase/storage";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -10,12 +11,13 @@ const firebaseConfig = {
   apiKey: "AIzaSyCinIggTeU5LL1_uRBtB5tIfAWWLvmYk1I",
   authDomain: "songapp-e0145.firebaseapp.com",
   projectId: "songapp-e0145",
-  storageBucket: "songapp-e0145.appspot.com",
+  // storageBucket: "songapp-e0145.appspot.com",
   messagingSenderId: "578887088792",
   appId: "1:578887088792:web:52a555055752ee306feeed",
-  measurementId: "G-RPKXL9RWTT"
+  measurementId: "G-RPKXL9RWTT",
+  storageBucket: "gs://songapp-e0145.appspot.com",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const storage = getStorage(app);
