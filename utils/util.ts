d10e2705ebbcfa173bce4f3b9e1dacbe4d2f4f3c
Kaartik Shukla <kaartikshukla7120@gmail.com>
2023-03-27 18:01:07 +0530

    Added a utility functjon for converting values to strings

--- utils/util.ts
+++ utils/util.ts
@@ -8,3 +8,10 @@ export function addImage(editor: Editor | null, url: string) {
 
     editor.chain().focus().setImage({ src: url }).run();
 }
+
+export function convertToString(value: any) {
+    if (typeof value === 'string') {
+        return value;
+    }
+    return JSON.stringify(value);
+}
