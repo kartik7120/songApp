68ac4d1a5fa7fcf257669e19456a51c242530507
Kaartik Shukla <kaartikshukla7120@gmail.com>
2023-03-29 13:41:08 +0530

    Added config for sass files

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
   reactStrictMode: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
 }
 
 module.exports = nextConfig;
