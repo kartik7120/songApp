c63a8369b30bddf8780df553ed9eaa301508c9d3
Kaartik Shukla <kaartikshukla7120@gmail.com>
2023-03-31 13:39:17 +0530

    Added a wrapper component for centering side controlls in the center

--- components/SidePostMenu.tsx
+++ components/SidePostMenu.tsx
@@ -8,40 +8,42 @@ import styles from "../styles/sidepostmenu.module.scss";
 
 export default function SidePostMenu() {
     return (
-        <aside className={styles.container}>
-            <ActionIcon size="xl" variant="transparent" className={styles.btnContainer}>
-                <RiHeartAddLine size={30} className={clsx(styles.iconClass, styles.heartClass)} />
-                <Text component="span" >0</Text>
-            </ActionIcon>
-            <ActionIcon size="xl" variant="transparent" className={styles.btnContainer}>
-                <FaRegCommentAlt size={30} id="comment" className={clsx(styles.iconClass, styles.commentClass)} />
-                <Text component="span" >0</Text>
-            </ActionIcon>
-            <ActionIcon size="xl" variant="transparent" className={styles.btnContainer}>
-                <BsBookmark size={30} id="save" className={clsx(styles.iconClass, styles.bookmarkClass)} />
-                <Text component="span" >0</Text>
-            </ActionIcon>
-            <Menu shadow="md" position="right" withArrow={false} >
-                <Menu.Target>
-                    <ActionIcon size="xl">
-                        <SlOptions />
-                    </ActionIcon>
-                </Menu.Target>
-                <Menu.Dropdown>
-                    <Menu.Item>
-                        <Group position="apart">
-                            <Text>Copy link</Text>
-                            <VscCopy />
-                        </Group>
-                    </Menu.Item>
-                    <Menu.Item>Share on Twitter</Menu.Item>
-                    <Menu.Item>Share on Facebook</Menu.Item>
-                    <Menu.Item>Share on LinkedIn</Menu.Item>
-                    <Menu.Item>Share on HackerNews</Menu.Item>
-                    <Menu.Item>Share on Reddit</Menu.Item>
-                    <Menu.Item>Share post via...</Menu.Item>
-                </Menu.Dropdown>
-            </Menu>
-        </aside>
+        <div className={styles.wrapper}>
+            <aside className={styles.container}>
+                <ActionIcon size="xl" variant="transparent" className={styles.btnContainer}>
+                    <RiHeartAddLine size={30} className={clsx(styles.iconClass, styles.heartClass)} />
+                    <Text component="span" >0</Text>
+                </ActionIcon>
+                <ActionIcon size="xl" variant="transparent" className={styles.btnContainer}>
+                    <FaRegCommentAlt size={30} id="comment" className={clsx(styles.iconClass, styles.commentClass)} />
+                    <Text component="span" >0</Text>
+                </ActionIcon>
+                <ActionIcon size="xl" variant="transparent" className={styles.btnContainer}>
+                    <BsBookmark size={30} id="save" className={clsx(styles.iconClass, styles.bookmarkClass)} />
+                    <Text component="span" >0</Text>
+                </ActionIcon>
+                <Menu shadow="md" position="right" withArrow={false} >
+                    <Menu.Target>
+                        <ActionIcon size="xl">
+                            <SlOptions />
+                        </ActionIcon>
+                    </Menu.Target>
+                    <Menu.Dropdown>
+                        <Menu.Item>
+                            <Group position="apart">
+                                <Text>Copy link</Text>
+                                <VscCopy />
+                            </Group>
+                        </Menu.Item>
+                        <Menu.Item>Share on Twitter</Menu.Item>
+                        <Menu.Item>Share on Facebook</Menu.Item>
+                        <Menu.Item>Share on LinkedIn</Menu.Item>
+                        <Menu.Item>Share on HackerNews</Menu.Item>
+                        <Menu.Item>Share on Reddit</Menu.Item>
+                        <Menu.Item>Share post via...</Menu.Item>
+                    </Menu.Dropdown>
+                </Menu>
+            </aside>
+        </div>
     )
 }
