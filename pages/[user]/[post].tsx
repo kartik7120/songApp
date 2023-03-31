d4eb101213bcd4e2d0bff0a00080c7d0ecf7ab74
Kaartik Shukla <kaartikshukla7120@gmail.com>
2023-03-31 13:44:32 +0530

    Added a post route that will render with a blog post

--- pages/[user]/[post].tsx
+++ pages/[user]/[post].tsx
@@ -1,10 +1,14 @@
+import Blog from "@/components/Blog";
 import SidePostMenu from "@/components/SidePostMenu";
+import UserInfo from "@/components/UserInfo";
+import styles from "../../styles/postHome.module.scss";
 
 export default function BlogPost() {
     return (
-        <>
-            <h1>This is my blog post</h1>
+        <div className={styles.container}>
             <SidePostMenu />
-        </>
+            <Blog />
+            <UserInfo />
+        </div>
     )
 }
