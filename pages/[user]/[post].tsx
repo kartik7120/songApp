4776dcf6cabb70295902964c213bf38a04d7fe09
Kaartik Shukla <kaartikshukla7120@gmail.com>
2023-04-03 19:04:44 +0530

    Added a trpc query for fetching data for a single blog post

--- pages/[user]/[post].tsx
+++ pages/[user]/[post].tsx
@@ -2,12 +2,30 @@ import Blog from "@/components/Blog";
 import SidePostMenu from "@/components/SidePostMenu";
 import UserInfo from "@/components/UserInfo";
 import styles from "../../styles/postHome.module.scss";
+import { useRouter } from "next/router";
+import { trpc } from "@/utils/trpc";
 
 export default function BlogPost() {
+
+    const router = useRouter();
+    const { user, post } = router.query;
+    const { data, isError, error, isLoading, isPaused } =
+        trpc.getBlogPost.useQuery({ id: post as string, uid: user as string }, {
+            enabled: !!user && !!post
+        });
+
+    if (isError) {
+        return <div>{error.message}</div>
+    }
+
+    if (isLoading) {
+        return <div>Loading...</div>
+    }
+
     return (
         <div className={styles.container}>
             <SidePostMenu />
-            <Blog />
+            <Blog blogImage={data.blogImage} body={data.body} title={data.title} tags={data.tags} id={data.id} />
             <UserInfo />
         </div>
     )
