1f4f1a2b40c2da2559efac49d71ddbb8c1a019a4
Kaartik Shukla <kaartikshukla7120@gmail.com>
2023-04-03 19:07:35 +0530

    added props types using infer Router output types

--- components/Blog.tsx
+++ components/Blog.tsx
@@ -1,11 +1,16 @@
-import { Avatar, Badge, Paper, Stack, Text, Title } from "@mantine/core";
+import { Avatar, Badge, Paper, Text, Title } from "@mantine/core";
 import { Image } from '@mantine/core';
 import Link from "next/link";
 import styles from "../styles/blog.module.scss";
 import { Divider } from '@mantine/core';
 import CommentSection from "./CommentSection";
+import parse from 'html-react-parser';
+import { inferRouterOutputs } from "@trpc/server";
+import { AppRouter } from "@/server/routers/_app";
 
-export default function Blog() {
+type Props = inferRouterOutputs<AppRouter>["getBlogPost"];
+
+export default function Blog(props: Props) {
     return (
         <Paper withBorder style={{ maxWidth: 1000 }} radius="md">
             <div>
@@ -26,23 +31,19 @@ export default function Blog() {
                     </div>
                 </div>
                 <Title order={1} style={{ marginBottom: "0.5em", wordBreak: "break-word", marginTop: "0.5em" }}>
-                    Title of the blogpost will go here
+                    {props.title}
                 </Title>
                 <div className={styles.badgeContainer}>
-                    <Badge size="lg">Web</Badge>
-                    <Badge size="lg">Web</Badge>
-                    <Badge size="lg">Web</Badge>
-                    <Badge size="lg">Web</Badge>
+                    {props.tags && props.tags.map((tag, index) => (
+                        <Badge key={index} size="lg" color="blue" variant="filled">
+                            {tag}
+                        </Badge>
+                    ))}
                 </div>
             </div>
             <div className={styles.container2}>
                 <Text style={{ padding: "32px 64px" }}>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis voluptate nulla doloremque at! Deleniti eos dolorum, non expedita delectus provident explicabo velit aut rerum. Porro commodi fugiat nesciunt recusandae perferendis.
-                    Minus ipsam libero eligendi maiores et iusto veniam nihil dolorum voluptatum, consequatur eum excepturi, dicta ipsum atque consequuntur tempore qui? Sed, labore dicta. Debitis nobis iure necessitatibus laudantium aperiam optio.
-                    Dolore itaque aspernatur tempore est ab hic quibusdam porro? Ipsa aut distinctio cumque alias odio qui dolorem ipsum eius culpa nemo quidem tenetur molestias ab magni, doloremque blanditiis commodi perferendis?
-                    Nisi magnam suscipit nesciunt. Corrupti labore culpa quibusdam fugit assumenda vitae cum autem illo provident qui, atque suscipit! Veniam labore est vero tempore fugiat eveniet minima consectetur suscipit. Deleniti, adipisci.
-                    Nisi quis sequi dolores exercitationem alias, officiis dolore adipisci modi sapiente repellat optio perspiciatis, nostrum iusto et consequatur similique veritatis quia aliquid at, cupiditate explicabo vero architecto doloribus. Nobis, nemo.
-                    Ducimus laborum, eos eaque nesciunt incidunt praesentium omnis repellendus delectus. Autem modi nulla expedita mollitia sunt veniam consectetur est corporis incidunt? Inventore recusandae quia distinctio, odio repellat ea dolores suscipit.
+                    {parse(props.body)}
                 </Text>
                 <Divider my="md" />
                 <CommentSection />
