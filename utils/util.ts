6ea9418aac8d7fa61f71f54ec24b2522cc737514
Kaartik Shukla <kaartikshukla7120@gmail.com>
2023-04-04 12:33:10 +0530

    added a function to load and upload blog image and blog itself

--- utils/util.ts
+++ utils/util.ts
@@ -1,5 +1,7 @@
 import { Editor } from "@tiptap/react";
-
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
+import { db, storage } from "../firebase";
+import { DocumentData, DocumentReference, addDoc, collection } from "firebase/firestore";
 export function addImage(editor: Editor | null, url: string) {
 
     if (editor === null) {
@@ -14,4 +16,40 @@ export function convertToString(value: any) {
         return value;
     }
     return JSON.stringify(value);
+}
+
+export function uploadBlogImage(image: File | null, uid: string) {
+    return new Promise(async (resolve, reject) => {
+        if (image === null) {
+            resolve("Image is null");
+        }
+        const mountainRef = ref(storage, `${uid}/images/${image?.name}`);
+        await uploadBytes(mountainRef, image!).then((snapshot) => {
+            getDownloadURL(snapshot.ref).then((downloadURL) => {
+                console.log('File available at', downloadURL);
+                resolve(downloadURL);
+            });
+        }).catch((error) => {
+            reject(error)
+            console.log(error);
+        })
+    });
+}
+
+export function uploadBlog(uid: string, data: any, image: File | null) {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const imageUrl = await uploadBlogImage(image, uid);
+            const docRef = await addDoc(collection(db, "users", uid, "blogs"), {
+                title: data.title,
+                tags: data.tags,
+                body: data.body,
+                blogImage: imageUrl === "Image us null" ? null : imageUrl,
+            });
+            resolve(docRef as DocumentReference<DocumentData>);
+        } catch (error) {
+            reject(error);
+            console.log(error);
+        }
+    });
 }
