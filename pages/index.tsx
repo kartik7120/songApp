e7cee6e4601e7569941906bc7cd4f577b6712465
Kaartik Shukla <kaartikshukla7120@gmail.com>
2023-04-06 16:01:42 +0530

    Removed the user info component from the index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,7 +18,6 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <UserInfo />
         <Link href={`/user/1/`}>Go to post</Link>
       </main>
     </>
