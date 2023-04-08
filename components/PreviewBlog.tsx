dd247fa1dfb9269e86921f045f1236c8c23cf26b
Kaartik Shukla <kaartikshukla7120@gmail.com>
2023-04-08 10:08:03 +0530

    Added id prop to the Props interface

--- components/PreviewBlog.tsx
+++ components/PreviewBlog.tsx
@@ -9,6 +9,7 @@ interface Props {
         description: string;
         body: string;
         tags: string[];
+        id: string
     }
 };
 
@@ -16,7 +17,7 @@ export default function PreviewBlog({ blog }: Props) {
     const user = auth.currentUser;
 
     return (
-        <Paper shadow="md" withBorder p="xl">
+        <Paper shadow="md" withBorder p="xl" component="a" href={`/${user!.uid}/${blog.id}`}>
             <div className={styles.container1}>
                 <Avatar src={user?.photoURL || "https://avatars.githubusercontent.com/u/18677354?v=4"} size="md" radius="xl" />
                 <Text size="xl">{user && user?.displayName}</Text>
