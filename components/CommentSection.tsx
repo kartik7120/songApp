91698dfde2d6d9ca9d3c5a8af7bad025b4fb3c77
Kaartik Shukla <kaartikshukla7120@gmail.com>
2023-04-08 17:15:59 +0530

    Added targetRef to the props of comment Section

--- components/CommentSection.tsx
+++ components/CommentSection.tsx
@@ -2,10 +2,14 @@ import { Avatar, Button, Group } from "@mantine/core";
 import EditorCom from "./EditorComponent";
 import styles from "../styles/comment.module.scss";
 
-export default function CommentSection() {
+interface Props {
+    targetRef: any
+}
+
+export default function CommentSection(props:Props) {
 
     return (
-        <div className={styles.container}>
+        <div className={styles.container} ref={props.targetRef}>
             <Avatar src={"https://avatars.githubusercontent.com/u/25126241?v=4"} size="md" radius="xl" />
             <EditorCom />
         </div>
