f1157a7ecf11933ca86872dec7e48fc6e93c4ba2
Kaartik Shukla <kaartikshukla7120@gmail.com>
2023-04-08 14:15:28 +0530

    added scrollIntoView function

--- pages/[user]/[post]/index.tsx
+++ pages/[user]/[post]/index.tsx
@@ -4,6 +4,7 @@ import UserInfo from "@/components/UserInfo";
 import styles from "../../../styles/postHome.module.scss";
 import { useRouter } from "next/router";
 import { trpc } from "@/utils/trpc";
+import { useScrollIntoView } from "@mantine/hooks";
 
 export default function BlogPost() {
 
@@ -11,8 +12,11 @@ export default function BlogPost() {
     const { user, post } = router.query;
     const { data, isError, error, isLoading, isPaused } =
         trpc.getBlogPost.useQuery({ id: post as string, uid: user as string }, {
-            enabled: !!user && !!post
+            enabled: !!user && !!post,
+            queryKey: ["getBlogPost", { id: post as string, uid: user as string }],
         });
+
+    const { scrollIntoView, targetRef } = useScrollIntoView<HTMLDivElement>();
 
     if (isError) {
         return <div>{error.message}</div>
@@ -24,8 +28,8 @@ export default function BlogPost() {
 
     return (
         <div className={styles.container}>
-            <SidePostMenu />
-            <Blog blogImage={data.blogImage} body={data.body} title={data.title} tags={data.tags} id={data.id} />
+            <SidePostMenu scrollIntoView={scrollIntoView} postId={post as string} userId={user as string} reactionCount={data.reactions && data.reactions.length} />
+            <Blog targetRef={targetRef} blogImage={data.blogImage} body={data.body} title={data.title} tags={data.tags} id={data.id} />
             <UserInfo />
         </div>
     )
