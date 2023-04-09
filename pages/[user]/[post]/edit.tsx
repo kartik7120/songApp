92afd8f90165f89de82aee39cd46456cf1371cc1
Kaartik Shukla <kaartikshukla7120@gmail.com>
2023-04-09 13:09:38 +0530

    added editor state to the edit component

--- pages/[user]/[post]/edit.tsx
+++ pages/[user]/[post]/edit.tsx
@@ -1,7 +1,23 @@
+import FullEditor from "@/components/FullEditor";
+import { Editor } from "@tiptap/react";
+import { useRef } from "react";
+import { createContext, useEffect, useState } from "react";
+
+export const Context = createContext<any | null>(null);
+
 export default function EditPost() {
+    const editor = useRef<Editor | null>(null);
+    const [editorState, setEditorState] = useState<Editor | null>(null);
+
+    useEffect(() => {
+        if (editorState) {
+            console.log(editorState.getHTML());
+        }
+    }, [editorState])
+
     return (
-        <div>
-            <h1>Edit Post</h1>
-        </div>
+        <Context.Provider value={setEditorState}>
+            <FullEditor />
+        </Context.Provider>
     );
 }
