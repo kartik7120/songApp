eb3e2670ff2c9a414ff173cac086b9d94b9e0929
Kaartik Shukla <kaartikshukla7120@gmail.com>
2023-04-09 09:48:46 +0530

    Added the link component to 'add comment' component

--- components/PreviewBlog.tsx
+++ components/PreviewBlog.tsx
@@ -2,6 +2,7 @@ import { Avatar, Badge, Button, Paper, Text } from "@mantine/core";
 import { FaRegComment } from "react-icons/fa";
 import styles from "../styles/PreviewBlog.module.scss";
 import { auth } from "@/firebase";
+import Link from "next/link";
 
 interface Props {
     blog: {
@@ -9,7 +10,8 @@ interface Props {
         description: string;
         body: string;
         tags: string[];
-        id: string
+        id: string;
+        uid: string;
     }
 };
 
@@ -17,20 +19,24 @@ export default function PreviewBlog({ blog }: Props) {
     const user = auth.currentUser;
 
     return (
-        <Paper shadow="md" withBorder p="xl" component="a" href={`/${user!.uid}/${blog.id}`}>
+        <Paper shadow="md" withBorder p="xl" >
             <div className={styles.container1}>
                 <Avatar src={user?.photoURL || "https://avatars.githubusercontent.com/u/18677354?v=4"} size="md" radius="xl" />
                 <Text size="xl">{user && user?.displayName}</Text>
             </div>
             <div className={styles.container2}>
-                <Text mt={10} weight="bold" size='xl'>{blog.title}</Text>
+                <Text component="a" href={`/${user!.uid}/${blog.id}`} mt={10} weight="bold" size='xl'>{blog.title}</Text>
                 <div className={styles.badgeContainer}>
                     {blog.tags.map((tag, index) => (
                         <Badge color="red" size="lg" key={index}>{tag}</Badge>
                     ))}
                 </div>
             </div>
-            <Button mt={12} variant="subtle" leftIcon={<FaRegComment />}>Add comment</Button>
+            <Link href={`/${blog.uid}/${blog.id}#commentSection`}>
+                <Button mt={12} variant="subtle" leftIcon={<FaRegComment />}>
+                    Add comment
+                </Button>
+            </Link>
         </Paper>
     );
 }
