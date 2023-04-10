cb75abfc8eefd85bfccd2c732340b12b60562941
Kaartik Shukla <kaartikshukla7120@gmail.com>
2023-04-10 16:22:48 +0530

    Passing data for a particular blog post down Context Provider

--- pages/[user]/[post]/edit.tsx
+++ pages/[user]/[post]/edit.tsx
@@ -1,13 +1,22 @@
 import FullEditor from "@/components/FullEditor";
+import { trpc } from "@/utils/trpc";
 import { Editor } from "@tiptap/react";
+import Head from "next/head";
+import { useRouter } from "next/router";
 import { useRef } from "react";
 import { createContext, useEffect, useState } from "react";
 
 export const Context = createContext<any | null>(null);
 
 export default function EditPost() {
-    const editor = useRef<Editor | null>(null);
+    const router = useRouter();
+    const { user, post } = router.query;
     const [editorState, setEditorState] = useState<Editor | null>(null);
+    const { data, isLoading, isError, error, isSuccess, } = trpc.getBlogPost.useQuery({
+        id: post!! as string,
+        uid: user!! as string,
+        isDraft: false
+    });
 
     useEffect(() => {
         if (editorState) {
@@ -16,8 +25,13 @@ export default function EditPost() {
     }, [editorState])
 
     return (
-        <Context.Provider value={setEditorState}>
-            <FullEditor />
-        </Context.Provider>
-    );
+        <>
+            <Head>
+                <title>Edit post</title>
+            </Head>
+            <Context.Provider value={{ setEditorState, data, isEdit: true, isSuccess }}>
+                <FullEditor />
+            </Context.Provider>
+        </>
+    )
 }
