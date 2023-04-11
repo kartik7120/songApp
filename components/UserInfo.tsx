fb27261890bebbd5ac7586fb1b9571e8e3e2b7cd
Kaartik Shukla <kaartikshukla7120@gmail.com>
2023-04-11 11:33:27 +0530

    Added a div to the whole UserInfo component

--- components/UserInfo.tsx
+++ components/UserInfo.tsx
@@ -3,31 +3,33 @@ import styles from "../styles/userinfo.module.scss";
 
 export default function UserInfo() {
     return (
-        <Paper withBorder radius="md" p="md" style={{ gridColumn: "3/-1", maxHeight: "min-content" }}>
-            <div className={styles.container}>
-                <div className={styles.avatarDiv}>
-                    <Avatar style={{ marginRight: "1em" }} radius="xl" src={null} size="lg" />
-                    <Text style={{ marginTop: "0.8em" }} size="xl">Kartik Shukla</Text>
+        <div>
+            <Paper withBorder radius="md" p="md" style={{ gridColumn: "3/-1", maxHeight: "min-content" }}>
+                <div className={styles.container}>
+                    <div className={styles.avatarDiv}>
+                        <Avatar style={{ marginRight: "1em" }} radius="xl" src={null} size="lg" />
+                        <Text style={{ marginTop: "0.8em" }} size="xl">Kartik Shukla</Text>
+                    </div>
+                    <Button variant="filled" size="xl" color="violet" fullWidth>Follow</Button>
+                    <Text>Some bio text</Text>
+                    <div>
+                        <Text weight="bold">Location</Text>
+                        <Text>Califonia</Text>
+                    </div>
+                    <div>
+                        <Text weight="bold">Education</Text>
+                        <Text>Some college name</Text>
+                    </div>
+                    <div>
+                        <Text weight="bold">Work</Text>
+                        <Text>Some company name</Text>
+                    </div>
+                    <div>
+                        <Text weight="bold">Joined</Text>
+                        <Text>Some date</Text>
+                    </div>
                 </div>
-                <Button variant="filled" size="xl" color="violet" fullWidth>Follow</Button>
-                <Text>Some bio text</Text>
-                <div>
-                    <Text weight="bold">Location</Text>
-                    <Text>Califonia</Text>
-                </div>
-                <div>
-                    <Text weight="bold">Education</Text>
-                    <Text>Some college name</Text>
-                </div>
-                <div>
-                    <Text weight="bold">Work</Text>
-                    <Text>Some company name</Text>
-                </div>
-                <div>
-                    <Text weight="bold">Joined</Text>
-                    <Text>Some date</Text>
-                </div>
-            </div>
-        </Paper>
+            </Paper>
+        </div>
     );
 }
