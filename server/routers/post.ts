6ca2dff371b9dd3588f3b16a1003dc3c5f72995b
Kaartik Shukla <kaartikshukla7120@gmail.com>
2023-04-11 21:55:22 +0530

    Added a procedure for fetching for saved blog posts for a user

--- server/routers/post.ts
+++ server/routers/post.ts
@@ -1,6 +1,6 @@
 import { z } from "zod";
 import { router, procedure } from "../trpc";
-import { addDoc, arrayUnion, collection, doc, getDoc, increment, updateDoc } from "firebase/firestore";
+import { addDoc, arrayUnion, collection, doc, getDoc, getDocs, increment, updateDoc } from "firebase/firestore";
 import { db } from "@/firebase";
 import { TRPCError } from "@trpc/server";
 
@@ -34,7 +34,7 @@ const postRouter = router({
                 postId: input.postId,
                 userId: input.userId,
             });
-            
+
             const docRef2 = doc(db, "users", input.userId, "blogs", input.postId);
             await updateDoc(docRef2, {
                 saves: increment(1),
@@ -70,6 +70,23 @@ const postRouter = router({
             });
 
             return "Post updated successfully";
+        } catch (error) {
+            throw new TRPCError({
+                code: 'INTERNAL_SERVER_ERROR',
+                message: 'Internal Server Error',
+            });
+        }
+    }),
+    getSavePosts: procedure.input(z.object({
+        userId: z.string().nullable(),
+    })).query(async ({ input }) => {
+        if (input.userId === null) {
+            return null;
+        }
+        try {
+            const docRef = collection(db, "users", input.userId, "saved");
+            const docSnap = await getDocs(docRef);
+            return docSnap.docs.map(doc => ({ ...doc.data(), id: doc.id }));
         } catch (error) {
             throw new TRPCError({
                 code: 'INTERNAL_SERVER_ERROR',
