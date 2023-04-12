885fc4de2d93fefa0a4fd98c1d0fa03b83e7318d
Kaartik Shukla <kaartikshukla7120@gmail.com>
2023-04-12 15:56:07 +0530

    Added readList component to render all the save posts of a user

--- pages/readingList.tsx
+++ pages/readingList.tsx
@@ -1,7 +1,8 @@
-import { Avatar, Paper, Text } from "@mantine/core";
+import { Avatar, Badge, Paper, Text, Title } from "@mantine/core";
 import styles from "../styles/reading.module.scss";
 import { trpc } from "@/utils/trpc";
 import { auth } from "@/firebase";
+import Link from "next/link";
 
 export default function ReadingList() {
     const user = auth.currentUser;
@@ -12,32 +13,39 @@ export default function ReadingList() {
     if (isError) return <div>Error: {error.message}</div>
 
     return (
-        <div>
-            <Paper p="md" withBorder>
-                <div className={styles.container}>
-                    <div>
-                        <Avatar src="https://pbs.twimg.com/profile_images/1361030000/IMG_0001_400x400.jpg" />
-                    </div>
-                    <div className={styles.container2}>
+        <div className={styles.wrapper}>
+            <Title order={1} mb={15}>Reading List ({data && data?.length})</Title>
+            {data && data.map((post) => (
+                <Paper p="md" withBorder key={post.id} mb={25}>
+                    <div className={styles.container}>
                         <div>
-                            <Text size="xl" weight={700}>
-                                Blog Title
-                            </Text>
+                            <Avatar src="https://pbs.twimg.com/profile_images/1361030000/IMG_0001_400x400.jpg" />
                         </div>
-                        <div className={styles.container3}>
-                            <Text size="md" weight={500}>
-                                Blog Author
-                            </Text>
-                            <Text size="md" weight={500}>
-                                Tags
-                            </Text>
-                            <Text>
-                                Date
-                            </Text>
+                        <div className={styles.container2}>
+                            <div>
+                                <Link href={`/${post.userId}/${post.id}`} style={{ textDecoration: "none", color: "unset" }}>
+                                    <Text size="xl" weight={700}>
+                                        {post.title}
+                                    </Text>
+                                </Link>
+                            </div>
+                            <div className={styles.container3}>
+                                <Text size="md" weight={500}>
+                                    Blog Author
+                                </Text>
+                                <Text size="md" weight={500}>
+                                    {post.tags && post.tags.map((tag) => (
+                                        <Badge mr={7} key={tag}>{tag}</Badge>
+                                    ))}
+                                </Text>
+                                <Text>
+                                    Date
+                                </Text>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </Paper>
+                </Paper>
+            ))}
         </div>
     );
 }
