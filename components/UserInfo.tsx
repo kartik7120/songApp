7b08b7239366c14409645587d8b6e557d4d5ceb1
Kaartik Shukla <kaartikshukla7120@gmail.com>
2023-04-17 14:42:57 +0530

    Conditionally rendering user information in UserInfo component

--- components/UserInfo.tsx
+++ components/UserInfo.tsx
@@ -1,7 +1,15 @@
 import { Avatar, Button, Paper, Text } from "@mantine/core";
 import styles from "../styles/userinfo.module.scss";
+import { trpc } from "@/utils/trpc";
+import { auth } from "@/firebase";
 
 export default function UserInfo() {
+
+    const user = auth.currentUser;
+    const { data } = trpc.user.getUserInfo.useQuery({
+        userUid: user?.uid!,
+    });
+
     return (
         <div>
             <Paper withBorder radius="md" p="md" style={{ gridColumn: "3/-1", maxHeight: "min-content" }}>
@@ -10,24 +18,35 @@ export default function UserInfo() {
                         <Avatar style={{ marginRight: "1em" }} radius="xl" src={null} size="lg" />
                         <Text style={{ marginTop: "0.8em" }} size="xl">Kartik Shukla</Text>
                     </div>
-                    <Button variant="filled" size="xl" color="violet" fullWidth>Follow</Button>
-                    <Text>Some bio text</Text>
+                    <Button variant="filled" size="md" color="violet" fullWidth>Follow</Button>
                     <div>
-                        <Text weight="bold">Location</Text>
-                        <Text>Califonia</Text>
+                        {data && data.bio &&
+                            <>  <Text>Bio</Text>
+                                <Text>{data.bio}</Text>
+                            </>}
                     </div>
                     <div>
-                        <Text weight="bold">Education</Text>
-                        <Text>Some college name</Text>
+                        {data && data.location && <>
+                            <Text weight="bold">Location</Text>
+                            <Text>{data.location}</Text>
+                        </>}
                     </div>
                     <div>
-                        <Text weight="bold">Work</Text>
-                        <Text>Some company name</Text>
+                        {data && data.education && <>
+                            <Text weight="bold">Education</Text>
+                            <Text>{data.education}</Text>
+                        </>}
                     </div>
                     <div>
+                        {data && data.work && <>
+                            <Text weight="bold">Work</Text>
+                            <Text>{data.work}</Text>
+                        </>}
+                    </div>
+                    {data && data.joined && <div>
                         <Text weight="bold">Joined</Text>
                         <Text>Some date</Text>
-                    </div>
+                    </div>}
                 </div>
             </Paper>
         </div>
