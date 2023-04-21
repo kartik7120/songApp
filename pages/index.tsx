83d5d272ebc75896424e1d812eec7bd8bfe1691d
Kaartik Shukla <kaartikshukla7120@gmail.com>
2023-04-21 21:53:03 +0530

    Added a Tab component to render different tabs

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,9 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
-import UserInfo from '@/components/UserInfo'
-import Link from 'next/link'
-import SidePostMenu from '@/components/SidePostMenu'
+import ListeningsComponent from '@/components/Listenings'
+import styles from "../styles/index.module.scss";
+import { Tabs } from '@mantine/core';
+import PostedBlogs from '@/components/RecentlyPostedBlogs';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,8 +16,33 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <Link href={`/user/1/`}>Go to post</Link>
+      <main >
+        <div className={styles.container}>
+          <div>
+            <Tabs defaultValue="gallery">
+              <Tabs.List>
+                <Tabs.Tab value="gallery">Recently posted blogs</Tabs.Tab>
+                <Tabs.Tab value="messages">Messages</Tabs.Tab>
+                <Tabs.Tab value="settings">Settings</Tabs.Tab>
+              </Tabs.List>
+
+              <Tabs.Panel value="gallery" pt="xs">
+                <PostedBlogs />
+              </Tabs.Panel>
+
+              <Tabs.Panel value="messages" pt="xs">
+                Messages tab content
+              </Tabs.Panel>
+
+              <Tabs.Panel value="settings" pt="xs">
+                Settings tab content
+              </Tabs.Panel>
+            </Tabs>
+          </div>
+          <div>
+            <ListeningsComponent />
+          </div>
+        </div>
       </main>
     </>
   )
